f5e303dc2b1a1cf2ca746a770e4b904d7c9fb9ef
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2023-12-12 18:32:56 +0330

    implements aut middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { verify } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { JWT_SECRET } from "src/config";
+import { ExpressRequest } from "src/types/expressRequest.interface";
+import { UserService } from "src/user/user.service";
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+    constructor(private readonly userService: UserService){}
+    async use(req: ExpressRequest,res: Response,next: NextFunction){
+        if (!req.headers.authorization) {
+            req.user = null;
+            next();
+            return;
+        }
+        const token =req.headers.authorization.split(' ')[1];
+        try {
+            const decode = verify(token, JWT_SECRET);
+            const user =   await this.userService.findById(decode.id);
+            req.user=user;
+            next();
+        } catch (error) {
+            req.user= null;
+            next();
+        }
+    }
+}
