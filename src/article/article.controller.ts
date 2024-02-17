d5eecb550486b140dbaf18866464205da83bc70e
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2024-02-17 21:42:39 +0330

    create article method

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { ArticleService } from "./article.service";
+import { AuthGuard } from "src/user/guards/auth.guards";
+import { User } from "src/user/decoratores/user.decorator";
+import { CreateArticleDto } from "./dto/createArticle.dto";
+import { UserEntity } from "src/user/user.entity";
 
 @Controller('articles')
 export class ArticleController
 {
     constructor(private readonly articleService: ArticleService){}
  @Post()
- async create()   
+ @UseGuards(AuthGuard)
+ async create(@User()currentUser: UserEntity,
+ @Body('article')createArticleDto:CreateArticleDto): Promise<any>
  {
-    return this.articleService.createArticle();
+    return await this.articleService.createArticle(currentUser,createArticleDto);
  }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { CreateArticleDto } from "./dto/createArticle.dto";
+import { ArticleEntity } from "./article.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { UserEntity } from "src/user/user.entity";
 
 @Injectable()
 export class ArticleService{
-async createArticle()
+    constructor(
+        @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>){}
+async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto): Promise<ArticleEntity>
 {
-    return "Create Article Service";
+    const article = new ArticleEntity();
+    Object.assign(article,createArticleDto);
+    if(!article.tagList){
+        article.tagList=[];
+    }
+    article.slug='fooo';
+    article.author=currentUser;
+    return await this.articleRepository.save(article);
 }
 }
