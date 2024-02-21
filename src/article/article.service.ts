3b7893ffdc3b9e94ac7db8daec9702f985cf3953
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2024-02-21 19:05:35 +0330

    feat/install slugify and implements into the code

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -5,7 +5,8 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { UserEntity } from "src/user/user.entity";
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
-
+import slugify from "slugify";
+import { IsLowercase } from "class-validator";
 @Injectable()
 export class ArticleService{
     constructor(
@@ -20,13 +21,20 @@ async createArticle(
     if(!article.tagList){
         article.tagList=[];
     }
-    article.slug='fooo';
+    article.slug= this.getSlug(createArticleDto.title);
     article.author=currentUser;
     return await this.articleRepository.save(article);
 }
     buildArticleResponse(article: ArticleEntity): ArticleResponseInterface{
 
         return {article};
+    }
+
+    private getSlug(title: string): string{
+
+        return slugify(title, {lower: true})+ 
+        '-' +
+         ((Math.random() * Math.pow(36,6))|0).toString(36);
     }
 
 }
--- src/article/types/articleResponse.interface.ts
+++ src/article/types/articleResponse.interface.ts
@@ -3,5 +3,4 @@ import { ArticleEntity } from "../article.entity";
 export interface ArticleResponseInterface
 {
     article: ArticleEntity;
-
 }
